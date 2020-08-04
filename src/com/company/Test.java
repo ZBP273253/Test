7fe4f528eea31ff6b458d042346f82c837623a11
ZBP273253 <2732530938@qq.com>
2020-08-04 08:31:49 +0800

    新增

---
+++ src/com/company/Test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Test {
+    public static void aa(){
+        int i=10;
+        double a=3.6;
+        System.out.println(i+a);
+    }
+
+    public static void main(String[] args) {
+        aa();
+    }
+}
